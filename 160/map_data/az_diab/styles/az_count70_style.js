95ed21f1a14568d411675813251b27ebc1639baa
Natalie Marcom <nmarcom@ucdavis.edu>
2017-05-31 15:42:19 -0700

    Add files via upload

---
+++ 160/map_data/az_diab/styles/az_count70_style.js
@@ -0,0 +1,127 @@
+var size = 0;
+function categories_az_count70(feature, value) {
+                switch(value) {case '2':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(255,255,204,1.0)'})
+    })];
+                    break;
+case '6':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(226,244,197,1.0)'})
+    })];
+                    break;
+case '7':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(197,233,189,1.0)'})
+    })];
+                    break;
+case '8':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(168,221,182,1.0)'})
+    })];
+                    break;
+case '16':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(139,210,184,1.0)'})
+    })];
+                    break;
+case '17':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(109,199,189,1.0)'})
+    })];
+                    break;
+case '20':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(80,188,194,1.0)'})
+    })];
+                    break;
+case '23':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(62,174,194,1.0)'})
+    })];
+                    break;
+case '26':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(55,157,191,1.0)'})
+    })];
+                    break;
+case '32':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(49,140,187,1.0)'})
+    })];
+                    break;
+case '39':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(43,121,181,1.0)'})
+    })];
+                    break;
+case '121':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(41,98,170,1.0)'})
+    })];
+                    break;
+case '493':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(39,75,159,1.0)'})
+    })];
+                    break;
+default:
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(37,52,148,1.0)'})
+    })];
+                    break;}};
+var styleCache_az_count70={}
+var style_az_count70 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("groc_strs");
+    var style = categories_az_count70(feature, value);
+    var labelText = "";
+    var currentFeature = feature;
+    clusteredFeatures = feature.get("features");
+    if (typeof clusteredFeatures !== "undefined") {
+        if (size >= 2) {
+            labelText = size.toString()
+        } else {
+            labelText = ""
+        }
+        var key = value + "_" + labelText
+        if (!styleCache_az_count70[key]){
+            var text = new ol.style.Text({
+                  font: '10.725px \'MS Shell Dlg 2\', sans-serif',
+                  text: labelText,
+                  textAlign: "center",
+                  fill: new ol.style.Fill({
+                    color: 'rgba(0, 0, 0, 1)'
+                  }),
+                });
+            styleCache_az_count70[key] = new ol.style.Style({"text": text})
+        }
+    } else {
+        if (feature.get("diab_2010") !== null) {
+            labelText = String(feature.get("diab_2010"));
+        } else {
+            labelText = ""
+        }
+        var key = value + "_" + labelText
+        if (!styleCache_az_count70[key]){
+            var text = new ol.style.Text({
+                    font: '10.725px \'MS Shell Dlg 2\', sans-serif',
+                    text: labelText,
+                    textBaseline: "center",
+                    textAlign: "left",
+                    offsetX: 5,
+                    offsetY: 3,
+                    fill: new ol.style.Fill({
+                      color: 'rgba(0, 0, 0, 1)'
+                    }),
+                });
+            styleCache_az_count70[key] = new ol.style.Style({"text": text})
+        }
+    }
+    var allStyles = [styleCache_az_count70[key]];
+    allStyles.push.apply(allStyles, style);
+    return allStyles;
+};
